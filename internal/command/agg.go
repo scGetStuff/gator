e909a4c0aaae6306f1019a96092505886f127759
Scott <67354101+scGetStuff@users.noreply.github.com>
2025-06-02 17:15:31 -0400

    update agg command to do real work CH5 L1

--- internal/command/agg.go
+++ internal/command/agg.go
@@ -2,25 +2,61 @@ package command
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"html"
 	"log"
+	"time"
 
+	"github.com/scGetStuff/gator/internal/database"
 	"github.com/scGetStuff/gator/internal/rss"
 )
 
 func HandlerAgg(s *State, cmd Command) error {
-	rssFeed, err := rss.FetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
+
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("the agg handler expects a single argument, time Between Requests")
+	}
+
+	timeBetweenRequests, err := time.ParseDuration(cmd.Args[0])
+	if err != nil {
+		return err
+	}
+
+	ticker := time.NewTicker(timeBetweenRequests)
+	for ; ; <-ticker.C {
+		scrapeFeeds(s)
+	}
+}
+
+func scrapeFeeds(s *State) error {
+	feed, err := s.Db.GetNextFeedToFetch(context.Background())
+	if err != nil {
+		return fmt.Errorf("GetNextFeedToFetch() failed: %w", err)
+	}
+
+	err = s.Db.MarkFeedFetched(context.Background(), database.MarkFeedFetchedParams{
+		ID:            feed.ID,
+		LastFetchedAt: sql.NullTime{Time: time.Now().UTC(), Valid: true},
+	})
+	if err != nil {
+		return fmt.Errorf("MarkFeedFetched() failed: %w", err)
+	}
+
+	// fmt.Println(feed.Name)
+	// rssFeed, err := rss.FetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
+	rssFeed, err := rss.FetchFeed(context.Background(), feed.Url)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	printStuff(rssFeed)
 
 	return nil
 }
 
 func printStuff(rssFeed *rss.RSSFeed) {
+	fmt.Println()
+	fmt.Println()
 	fmt.Println(html.UnescapeString(rssFeed.Channel.Title))
 	fmt.Println(rssFeed.Channel.Link)
 	fmt.Println(html.UnescapeString(rssFeed.Channel.Description))
--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -11,3 +11,13 @@ ON f.user_id = u.id;
 
 -- name: GetFeed :one
 SELECT * FROM feeds WHERE url = $1;
+
+-- name: MarkFeedFetched :exec
+UPDATE feeds
+SET last_fetched_at = $2, updated_at = $2
+WHERE id = $1;
+
+-- name: GetNextFeedToFetch :one
+SELECT * FROM feeds
+ORDER BY last_fetched_at ASC NULLS FIRST
+LIMIT 1;
